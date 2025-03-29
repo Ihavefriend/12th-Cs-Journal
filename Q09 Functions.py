e8d6cf9ef6fe04b21ba84081486bb19bee0fbd64
ihavefriend <ihavefriend01@gmail.com>
2025-03-29 16:30:28 +0400

    Removed usage of key in sorted

--- Q09 Functions.py
+++ Q09 Functions.py
@@ -5,6 +5,16 @@ Write a program to accept the dictionary and counting the number of vehicles rel
 Also display all vehicles in alphabetical order with its brand name.
 """
 
+def sortbrand(veh):
+    veh_items = list(veh.items())
+    for i in range(len(veh_items)):
+        for j in range(i + 1, len(veh_items)):
+            # Compare the brand (first element of the nested list)
+            if veh_items[i][1][0] > veh_items[j][1][0]:
+                veh_items[i], veh_items[j] = veh_items[j], veh_items[i]
+
+    print(dict(veh_items))
+
 def vehoperations(veh):
     count2020 = 0
     for i in veh:
@@ -12,11 +22,6 @@ def vehoperations(veh):
             count2020+=1
 
     print(count2020)
-    def brand(x):
-        return x[1][0]
-
-    sortedveh = sorted(veh.items(),key=brand)
-    print(dict(sortedveh))
 
 veh = {
     "Sedan": ["Toyota", "Camry", 2020, 25000],
@@ -25,4 +30,4 @@ veh = {
     "Hatchback": ["Honda", "Civic", 2019, 22000]
 }
 vehoperations(veh)
-
+sortbrand(veh)
